0f45db8ed90b5d924936d1325bdb9d7d210d446f
Pavel Safonov <pavel.safonov.dev@gmail.com>
2025-03-22 21:41:49 +0400

    feat: add TailwindColorPicker

---
+++ src/view/main/ui/ColorPickerButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <Button
+    :class="[`bg-${color}-500 hover:bg-${color}-500`, { 'ring-2': active }]"
+    class="w-full h-8 rounded-lg hover:ring-2 ring-current block text-dark dark:text-white"
+  />
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+import { Button } from '@/components/ui/button';
+
+defineProps({
+  color: {
+    type: String as PropType<string>,
+    required: true,
+  },
+  active: {
+    type: Boolean as PropType<boolean>,
+    default: false,
+  },
+});
+</script>
---
+++ src/view/main/ui/TailwindColorPicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid grid-cols-5 grid-rows-2 pt-2 pb-2 gap-2">
+    <div
+      v-for="(color, index) in colors"
+      :key="index"
+    >
+      <ColorPickerButton
+        :color
+        :active="color === initialColor"
+        @click="$emit('update:initialColor', color)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType, ref, Ref } from 'vue';
+
+import ColorPickerButton from './ColorPickerButton.vue';
+
+defineProps({
+  initialColor: {
+    type: String as PropType<string>,
+    default: 'red',
+  },
+});
+
+defineEmits(['update:initialColor']);
+
+const colors: Ref<string[]> = ref([
+  'red',
+  'orange',
+  'yellow',
+  'lime',
+  'green',
+  'blue',
+  'purple',
+  'pink',
+  'gray',
+  'stone',
+]);
+</script>
