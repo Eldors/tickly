934cf522098c28eb6724ce266ef909fbd9592e29
Pavel Safonov <pavel.safonov.dev@gmail.com>
2025-03-22 21:43:06 +0400

    feat: add TaskEditorDialog

---
+++ src/view/main/ui/TaskEditorDialog.vue
@@ -0,0 +1,93 @@
+<template>
+  <Dialog
+    :open="open"
+    @update:open="$emit('update:open', $event)"
+  >
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{{ title }}</DialogTitle>
+      </DialogHeader>
+      <div>
+        <div class="grid w-full items-center gap-1.5">
+          <Label for="name">Name</Label>
+          <Input
+            id="name"
+            v-model="newTask.name"
+            class="w-fize-4ll"
+            placeholder="Name"
+          />
+        </div>
+        <div>
+          <Label>Color</Label>
+          <TailwindColorPicker v-model:initial-color="newTask.color" />
+        </div>
+      </div>
+      <DialogFooter>
+        <ConfirmButton
+          variant="destructive"
+          class="mr-auto"
+          @confirm="handleRemoveClick"
+        >
+          Delete
+        </ConfirmButton>
+        <Button @click="handleSaveClick">Save changes</Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+import { Button } from '@/components/ui/button';
+import { PropType, reactive, watchEffect } from 'vue';
+import { Task } from '@/types';
+import TailwindColorPicker from './TailwindColorPicker.vue';
+import ConfirmButton from '@/components/ui/confirm-button/ConfirmButton.vue';
+
+const props = defineProps({
+  title: {
+    type: String as PropType<string>,
+    default: 'Edit Task',
+  },
+  open: {
+    type: Boolean as PropType<boolean>,
+    default: false,
+  },
+  task: {
+    type: [Object, null] as PropType<Partial<Task> | null>,
+    default: () => ({}),
+  },
+});
+
+const emits = defineEmits(['update:open', 'save', 'delete']);
+
+const newTask: Partial<Task> = reactive({
+  ...props.task,
+});
+
+const closeDialog = () => emits('update:open', false);
+
+const handleSaveClick = () => {
+  emits('save', newTask);
+  closeDialog();
+};
+
+const handleRemoveClick = () => {
+  emits('delete', props.task?.id);
+  closeDialog();
+};
+
+watchEffect(() => {
+  Object.assign(newTask, props.task);
+});
+</script>
