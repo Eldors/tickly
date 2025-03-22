f8259483fe22dbd8bd126353915a39a17834934a
Pavel Safonov <pavel.safonov.dev@gmail.com>
2025-03-22 21:44:10 +0400

    feat: add update method to taskRepository

--- src/repositories/taskRepository.ts
+++ src/repositories/taskRepository.ts
@@ -61,7 +61,34 @@ export class TaskRepository {
     }
   }
 
-  // Пометка задачи как удаленной
+  async update(task: Partial<Task>) {
+    if (!db) {
+      throw new Error(DB_NOT_INITIALIZED);
+    }
+
+    if (!task.name) {
+      throw new Error(TASK_NAME_REQUIRED);
+    }
+
+    try {
+      await db.execute(
+        `
+          UPDATE 
+            tasks 
+          SET 
+            name = $2,
+            color = $3 
+          WHERE 
+            id = $1
+        `,
+        [task.id, task.name, task.color],
+      );
+    } catch (error) {
+      console.error(UPDATE_TASK_ERROR, error);
+      throw error;
+    }
+  }
+
   async delete(id: number): Promise<void> {
     if (!db) {
       throw new Error(DB_NOT_INITIALIZED);
