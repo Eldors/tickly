6e6e8b6b0df664bbac4e75e464bbd6bca9782f86
Pavel Safonov <pavel.safonov.dev@gmail.com>
2025-04-12 21:03:47 +0400

    feat: add database operation error message

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,10 +1,13 @@
 export const DB_NOT_INITIALIZED: string = 'The database is not initialized';
+export const DB_OPERATION_ERROR: string =
+  'Error processing database operation:';
 // task repository errors
 export const TASK_NAME_REQUIRED: string = 'Task name is required';
 export const FETCH_TASKS_ERROR: string = 'Error fetching tasks:';
 export const CREATE_TASK_ERROR: string = 'Error creating task:';
 export const UPDATE_TASK_ERROR: string = 'Error updating task:';
 export const DELETE_TASK_ERROR: string = 'Error deleting task:';
+export const UPDATE_TASK_ORDER_ERROR: string = 'Error updating task order:';
 // record repository errors
 export const FETCH_RECORDS_ERROR: string = 'Error fetching records:';
 export const STOP_RECORD_ERROR: string = 'Error stopping record:';
